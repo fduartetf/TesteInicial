c391ea453ee50fa29b74f40a85a6a2c911e1a2b2
fduartetf <fabricio.duarte@tecfinance.com.br>
2016-01-14 15:00:50 -0200

    primeiro teste com o Git

---
+++ Exemplo1/Exemplo1/calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo1
+{
+    class calculadora
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int Soma(int a, int b)
+        {
+            return a + b;
+
+        }
+    }
+}
